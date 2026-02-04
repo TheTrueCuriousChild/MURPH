45e2bc38f1ca21a2e550485b48f3c0f65dfa6700
Pakshal Solanki <pakshalsolanki26@gmail.com>
2026-02-05 01:59:47 +0530

    cleaning

--- tailwind.config.ts
+++
@@ -1,5 +0,0 @@
-// Tailwind CSS v4 does not use tailwind.config.ts
-// Configuration is done in CSS files using @theme directive
-// See app/globals.css for theme customization
-
-export default {};
